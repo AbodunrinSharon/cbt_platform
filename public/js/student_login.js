b0f4633bed9e3b50cb08f81a9e887352b0f633d7
AbodunrinSharon <abodunrinsharon@gmail.com>
2025-01-11 21:57:50 +0100

    Q

--- public/js/student_login.js
+++ public/js/student_login.js
@@ -12,13 +12,11 @@ EQuery(function () {
 
     EQuery(window).on('load', function () {
         var height = window.innerHeight - 110;
-        EQuery('main').css(`min-height: ${height}px;width: ${window.innerWidth}px`);
-        EQuery('footer').css(`width: ${window.innerWidth}px`);
+        EQuery('main').css(`min-height: ${height}px;`);
     });
   
     EQuery(window).on('resize', function () {
         var height = window.innerHeight - 110;
-        EQuery('main').css(`min-height: ${height}px;width: ${window.innerWidth}px`);
-        EQuery('footer').css(`width: ${window.innerWidth}px`);
+        EQuery('main').css(`min-height: ${height}px;`);
     });
 });
